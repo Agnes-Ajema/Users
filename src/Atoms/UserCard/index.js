3c3e5d7ef2109d13ae1e872eb396f1b235d74523
Agnes <agnesajema@gmail.com>
2024-06-20 11:03:18 +0300

    add files

---
+++ src/Atoms/UserCard/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+const  UserCard =({image, fullName,id }) => {
+    return(
+        <div id='pics'>
+            <img src={image} alt= {`${fullName} profile`}/>
+            <h3>{fullName}</h3>
+            <img/>
+            <h3></h3>
+        </div>
+    );
+};
+export default UserCard;
